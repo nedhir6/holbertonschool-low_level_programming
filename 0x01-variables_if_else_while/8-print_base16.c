880e1912d3761c70bace8e9fa512f2692255c71d
nedhir6 <nedhirb@gmail.com>
2020-02-06 15:38:51 +0100

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - void
+*Return: 0
+*/
+int main(void)
+{int n = 0;
+char ch1 = 'a';
+for (n = '0' ; n <= '9' ; n++)
+{putchar(n); }
+for (ch1 = 'a' ; ch1 <= 'f' ; ch1++)
+{putchar(ch1); }
+putchar('\n');
+return (0); }
