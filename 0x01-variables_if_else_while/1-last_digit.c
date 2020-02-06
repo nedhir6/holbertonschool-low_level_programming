b35beef353102a42929f996ec5914f8388a48931
nedhir6 <nedhirb@gmail.com>
2020-02-06 12:08:51 +0100

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,9 +12,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 d = n % 10;
 if (d > 5)
-{printf("last digit of %d is %d and is greater than 5", n, d); }
+{printf("last digit of %d is %d and is greater than 5\n", n, d); }
 else if (d == 0)
-{printf("last digit of %d is %d and is 0", n, d); }
+{printf("last digit of %d is %d and is 0\n", n, d); }
 else
-{printf("last digit of %d is %d and is less than 6 and not 0", n, d); }
+{printf("last digit of %d is %d and is less than 6 and not 0\n", n, d); }
 return (0); }
