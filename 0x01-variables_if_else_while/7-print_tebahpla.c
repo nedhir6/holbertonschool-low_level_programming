6a3de84b2449c4078b47f91d40fbe4ddc9a59716
nedhir6 <nedhirb@gmail.com>
2020-02-06 15:30:26 +0100

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - void
+*Return: 0
+*/
+int main(void)
+{char ch1 = 'z';
+char ch2 = '\n';
+for (ch1 = 'z' ; ch1 >= 'a' ; ch1--)
+{putchar(ch1); }
+{putchar(ch2); }
+return (0); }
