db8364d945435da0da3eef6db86e76b1c2876b43
nedhir6 <nedhirb@gmail.com>
2020-02-06 14:05:03 +0100

    5 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - void
+*Return: 0
+*/
+int main(void)
+{char ch1 = 'a';
+char ch2 = '\n';
+for (ch1 = '0' ; ch1 <= '9' ; ch1++)
+{putchar(ch1); }
+putchar(ch2);
+return (0); }
