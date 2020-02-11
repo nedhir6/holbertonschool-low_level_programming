1d643dad2c8988b35c73d0b0d4aaf99830111633
nedhir6 <nedhirb@gmail.com>
2020-02-11 09:31:24 +0100

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*print_alphabet - void
+*/
+void print_alphabet(void)
+{char ch1 = 'a';
+char ch2 = '\n';
+for (ch1 = 'a' ; ch1 <= 'z' ; ch1++)
+{_putchar(ch1); }
+{_putchar(ch2); }
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*print_alphabet-10 - void
+*/
+void print_alphabet_x10(void)
+{
+char ch1 = 'a';
+char ch2 = '\n';
+int i =0;
+for (i=0; i>9; i++)
+{for (ch1 = 'a' ; ch1 <= 'z' ; ch1++)
+{_putchar(ch1); }
+_putchar(ch2);
+}}
