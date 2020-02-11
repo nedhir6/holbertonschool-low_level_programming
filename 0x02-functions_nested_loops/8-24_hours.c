12fc6d1783f41f0c916b290e51f1465f3663b18e
nedhir6 <nedhirb@gmail.com>
2020-02-11 16:54:39 +0100

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 /**
 *jack_bauer - every minute
-*@n : int
 *Return: 1
 */
 void jack_bauer(void)
 {
 int d1 = 0;
 int d2 = 0;
-for (d1 = 0 ; d1 < 99 ; d1++)
+for (d1 = 0 ; d1 < 24 ; d1++)
 {
-for (d2 = d1 + 1; d2 < 100 ; d2++)
+for (d2 = d1 + 1; d2 < 60 ; d2++)
 {_putchar(d1 / 10 + '0');
 _putchar(d1 % 10 + '0');
 _putchar(':');
 _putchar(d2 / 10 + '0');
 _putchar(d2 % 10 + '0');
-if (d1 != 98 || d2 != 99)
-{_putchar('\n'); }}}
+_putchar('\n');
+}
+}
 }
