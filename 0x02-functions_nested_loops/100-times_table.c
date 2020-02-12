0bc412d6fec7d9d5c74fd3213cc5970a99accf90
nedhir6 <nedhirb@gmail.com>
2020-02-12 22:47:43 +0100

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,42 @@
-#include "holberton.h"
-/**
-*print_times_table - times table
-*@n: int
-*Return: 1
-*/
 void print_times_table(int n)
 {
 int d1 = 0;
 int d2 = 0;
 int d3 = 0;
-if (n >= 0 && n <= 1)
+
+if (n >= 0 && n <= 15)
 {
+while (d3 <= n)
+{
+d1 = 1;
+_putchar('0');
 while (d1 <= n)
 {
-d2 = 1;
-_putchar('0');
-while (d2 <= n)
-{
-d3 = d1 * d2;
+d2 = d1 * d3;
 _putchar(',');
-putchar(' ');
-if (d3 < 10)
+_putchar(' ');
+if (d2 < 10)
 {
 _putchar(' ');
 _putchar(' ');
-_putchar(d3 + '0');
+_putchar(d2 + '0');
 }
-else if (d3 > 99)
+else if (d2 > 99)
 {
-_putchar((d3 / 100) + '0');
-_putchar(((d3 / 10) % 10) + '0');
-_putchar((d3 % 10) + '0');
+_putchar((d2 / 100) + '0');
+_putchar(((d2 / 10) % 10) + '0');
+_putchar((d2 % 10) + '0');
 }
 else
 {
 _putchar(' ');
-_putchar((d3 / 10) + '0');
-_putchar((d3 % 10) + '0');
+_putchar((d2 / 10) + '0');
+_putchar((d2 % 10) + '0');
 }
-d2++;
+d1++;
 }
 _putchar('\n');
-d1++;
+d3++;
 }
 }
 }
