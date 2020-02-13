ab2adca03980ac538e73ad02079264f71496dd40
nedhir6 <nedhirb@gmail.com>
2020-02-13 21:32:46 +0100

    fizz-bizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 1; a < 100; a++)
+{
+if (a % 3 == 0 && a % 5 == 0)
+printf("FizzBuzz ");
+else if (a % 3 == 0)
+printf("Fizz ");
+else if (a % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", a);
+}
+printf("Buzz\n");
+return (0);
+}
