a8fd6e6ef1854967c14b9c58071d525ced6eda54
nedhir6 <nedhirb@gmail.com>
2020-02-13 11:40:18 +0100

    prints numbersfrom 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*print_numbers - void
+*/
+void print_numbers(void)
+{char ch1 = '0';
+char ch2 = '\n';
+for (ch1 = '0' ; ch1 <= '9' ; ch1++)
+{_putchar(ch1); }
+{_putchar(ch2); }
+}
