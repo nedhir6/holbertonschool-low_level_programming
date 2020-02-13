79e59bb1b8daf749e2948313137453597998c3c2
nedhir6 <nedhirb@gmail.com>
2020-02-13 12:25:36 +0100

    betty fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,7 +6,8 @@ void print_most_numbers(void)
 {char ch1 = '0';
 char ch2 = '\n';
 for (ch1 = '0' ; ch1 <= '9' ; ch1++)
-{if ((ch1 != '2') && (ch1 != '4'))
+{
+if ((ch1 != '2') && (ch1 != '4'))
 _putchar(ch1);
 }
 _putchar(ch2);
