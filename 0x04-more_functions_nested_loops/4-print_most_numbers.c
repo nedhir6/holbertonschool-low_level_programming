b132fdd0a5e3601284cbd972f15eafbc48881a13
nedhir6 <nedhirb@gmail.com>
2020-02-13 12:23:26 +0100

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+*print_most_numbers - void
+*/
+void print_most_numbers(void)
+{char ch1 = '0';
+char ch2 = '\n';
+for (ch1 = '0' ; ch1 <= '9' ; ch1++)
+{if ((ch1 != '2') && (ch1 != '4'))
+_putchar(ch1);
+}
+_putchar(ch2);
+}
