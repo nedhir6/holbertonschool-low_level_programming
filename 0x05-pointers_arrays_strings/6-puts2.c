9dc629f0359b0f7b73d5a012e8f2b895f13f98f6
nedhir6 <nedhirb@gmail.com>
2020-02-18 23:55:37 +0100

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*puts2 - prints every other charachter
+*@str: char
+*/
+void puts2(char *str)
+{
+int a = 0;
+
+for (a = 0; str[a]; a++)
+{
+if (a % 2 == 0)
+_putchar(str[a]);
+}
+_putchar('\n');
+}
