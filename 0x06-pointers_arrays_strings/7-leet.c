20663f04a1661227d495ad875838901f900d8028
nedhir6 <nedhirb@gmail.com>
2020-02-20 23:23:25 +0100

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+char *leet(char *s)
+{
+int i = 0, j;
+char d[] = {'A', 'E', 'O', 'T', 'L'};
+char c[] = {'4', '3', '0', '7', '1'};
+while (s[i] != '\0')
+{
+for (j = 0; s1[j] != '\0'; j++)
+{
+if (s[i] == d[j] || s[i] == d[j] + 32)
+{
+s[i] = c[j];
+}
+}
+i++;
+}
+return (s);
+}
