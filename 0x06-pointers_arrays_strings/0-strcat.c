5ee1612a229ff500d31e787a1725f89c52af43f6
nedhir6 <nedhirb@gmail.com>
2020-02-20 22:31:13 +0100

    fix2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,12 +7,12 @@
 */
 char *_strcat(char *dest, char *src)
 {
-int i = 0,j = 0;
+int i = 0, j = 0;
 while (dest[i] != '\0')
 i++;
-for (j=0;src[j] != '\0';j++)
+for (j = 0; src[j] != '\0'; j++)
 {
-dest[i+j] = src[j];
+dest[i + j] = src[j];
 }
 return (dest);
 }
