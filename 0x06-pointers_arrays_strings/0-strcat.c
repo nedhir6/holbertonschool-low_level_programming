0ddb7e060e1b30ddc5b1086114efd88165d6693a
nedhir6 <nedhirb@gmail.com>
2020-02-20 19:53:13 +0100

    more pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+/**
+*_strca - concatenates two strings
+*@dest: pointer to char
+*@src: pointer to char
+*/
+char *_strcat(char *dest, char *src)
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory contains more pointer and arrays
