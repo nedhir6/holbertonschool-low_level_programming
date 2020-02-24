f288cac5988d41a3472356366afd83eafca87f9a
nedhir6 <nedhirb@gmail.com>
2020-02-24 23:16:24 +0100

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,10 +7,11 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s++)
-{
-if (*s == c)
-return (s);
-}
-return (0);
+	do {
+		if (*s == c)
+		{
+			return (s);
+		}
+	} while (*s++);
+	return (0);
 }
