991cb0b4c8b564bd1c1407621633606a0c0f3b31
nedhir6 <nedhirb@gmail.com>
2020-03-01 18:17:29 +0100

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiplies two numbers.
+ *@argc: argc
+ *@argv: argv
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+	return (0);
+}
