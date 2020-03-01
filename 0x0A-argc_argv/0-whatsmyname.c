fee3736f613f896302d5ac0b6c5537de462fcdd9
nedhir6 <nedhirb@gmail.com>
2020-03-01 16:50:01 +0100

    init

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * 
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s",argv[0]);
+	return (argc);
+}
