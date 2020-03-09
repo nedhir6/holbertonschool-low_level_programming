04cc8ae399b8dd157ccdf37c010004ba0ad16bd9
nedhir6 <nedhirb@gmail.com>
2020-03-09 23:55:09 +0100

    doggo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/**
+*struct dog - struct
+*@name: name
+*@age: age
+*@owner: owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _HOLBERTON_H_ */
