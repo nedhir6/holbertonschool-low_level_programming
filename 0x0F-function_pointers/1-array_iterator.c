1b0013f82365f5b81ae7deb040255ee5a15c25d4
nedhir6 <nedhirb@gmail.com>
2020-03-15 00:56:18 +0100

    ff

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,10 +8,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || size == 0 || action == NULL)
-		return;
 	unsigned int i = 0;
 
+	if (array == NULL || size == 0 || action == NULL)
+		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
