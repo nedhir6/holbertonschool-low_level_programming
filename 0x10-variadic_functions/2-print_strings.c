b08781cf0942eabf8a271247d2a798502d303323
nedhir ben hammouda <47131076+nedhir6@users.noreply.github.com>
2020-03-14 13:18:05 +0100

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,6 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		c = va_arg(ap, char*);
 		if (c == NULL)
 			printf("(nil)");
+		else
 		printf("%s", c);
 		if (i != n - 1 && separator != NULL)
 		{
