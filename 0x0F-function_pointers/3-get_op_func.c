51f45a5905bd7ae8b0577ae215d11096e8ba1ab6
nedhir6 <nedhirb@gmail.com>
2020-03-15 04:08:37 +0100

    ffs

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * get_op_func - operator
+ *@s: char
+ *Return: int
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op && strlen(s) == 1)
+		{
+			return (ops[i].f);
+			i++;
+		}
+	}
+	printf("Error\n");
+	exit(99);
+}
