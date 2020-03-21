b133aa220f2431e330fe0fb33916873eab3ec6d8
nedhir6 <nedhirb@gmail.com>
2020-03-21 03:09:44 +0100

    fixing req

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,12 +11,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int i = 0;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+	while (str[i] != 0)
+		i++;
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = i;
 	new->next = *head;
 	*head = new;
 	return (new);
