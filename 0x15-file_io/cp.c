ed58d41593fc3a07d4e927dfa715ab3197aa50be
nedhir6 <nedhirb@gmail.com>
2020-03-31 23:10:28 +0100

    0

---
+++ 0x15-file_io/cp.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - copies the content of a file to another file
+ *
+ * Return: void
+ */
+void main(void)
+{
+}
