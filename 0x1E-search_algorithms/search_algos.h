4288cf435a0a1beba0b2400f5038560d0840d253
nedhir6 <nedhirb@gmail.com>
2021-02-16 12:34:55 +0100

    betty docum. fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,11 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 /**
-*Description: header file for serach algo project
+*linear_search - searches for a value in an array of integers
+*@array: array of integers
+*@size: size of array
+*@value: value to search
+*Return: index of value
 */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
